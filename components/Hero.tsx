27a8872d85c561ddec4d500192e853852e933a5f
ziarecheyjvr <ziarecheyjvr@gmail.com>
2026-02-19 15:31:18 +0800

    Fix hero GIF alignment: Increase vertical translation to align subject head with 'RULES' text

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -62,7 +62,7 @@ const Hero: React.FC = () => {
           <img
             src="/assets/videos/hq.gif"
             alt="Sofia Marbella"
-            className="w-auto h-full max-w-full md:max-w-xl max-h-[95vh] object-contain object-bottom z-20 scale-100 md:scale-120 origin-bottom md:origin-bottom-right md:translate-x-[15%] md:translate-y-[12%]"
+            className="w-auto h-full max-w-full md:max-w-xl max-h-[100vh] object-contain object-bottom z-20 scale-100 md:scale-125 origin-bottom md:origin-bottom-right md:translate-x-[20%] md:translate-y-[15%]"
           />
         </div>
       </div>
